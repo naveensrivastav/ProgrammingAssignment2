ad2c27d19dae0b7bb6f524e5369dae65712df381
Naveen Srivastav <me.naveen86@gmail.com>
2017-01-24 22:08:04 +0530

    Updated cacheMatrix,R

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,40 @@
 ## There are two function "makeCacheMatrix" which takes a 2 X 2 matrix and conver the same into special matrix and cache its inverse also and return both in a list.
 
-## "cacheSolve" takes a matrix which check wether the matrix passed == special matrix and its inverse is calculated.
-## if both are true cached Inverse matrix is printed or returned else the passed matrix will be converted into special matrix.
+## "cacheSolve" takes a list with two element returned by makeCacheMatrix
 
 
 
-## Here matrix as argument is converted to special matrix and its inverse matrix is also assigned to cacheinverse special matrix
-## and returns you the matrix and its inverse 
-
+## Here argument is matrix and converted to special matrix. Its inverse is calculated and is cached.
+## This function returned a list of two element both are matrix, one is the converted special matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-splmatrix <<- x
+  splmatrix <<- x
   cacheinverse <<- solve(x)
- 
- list(splmatrix = splmatrix, cacheinverse = cacheinverse)
+  
+  list(splmatrix = splmatrix, cacheinverse = cacheinverse)
 }
 
 
-## This function again takes a 2 X 2 matrix and check if its already there in cache and inverse is calculated 
-## if TRUE cached inverse is returned  else the matrix passed will be turned into special matrix and its inverse will be calculated through
-## makeCacheMatrix  and invere is returned.
+## This function takes a list with two element x$splmatrix and x$cacheinverse.
+## x$splmatrix checks if its the same matrix and its cached inverse in not NULL.
 
 cacheSolve <- function(x, ...) {
-       
-  if( !is.null(cacheinverse) && x == splmatrix )
-     
-          message(" Getting Cached Data")
-          return(cacheinverse)
-  else
+  
+  if( !is.null(cacheinverse) && x$splmatrix == splmatrix )
+    
+   { 
+  print(" Getting Cached Data ...")
+  x$cacheinverse
+  return(x$cacheinverse)
+   }
+  
+    else
   {
-   ## When the passed matrix is not cached
-      temp <- makeCacheMatrix(x)
-   
-   ## Return a matrix that is the inverse of 'x'
-      temp$cacheinverse
+    ## When the passed matrix is not cached
+    temp <- makeCacheMatrix(x$splmatrix)
+    
+    ## Return a matrix that is the inverse of 'x'
+    temp$cacheinverse
   }  
-              
+  
 }
